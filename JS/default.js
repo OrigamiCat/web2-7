dfa63783d7bff6499464d4208fc72c0a1585a9c8
Origami3 <35678001+OrigamiCat@users.noreply.github.com>
2018-11-19 16:50:57 +0900

    Update default.js

--- JS/default.js
+++ JS/default.js
@@ -6,13 +6,13 @@ var number = math.floor(Math.random()*3);
 var answer = parseInt(window.prompt('数あてゲーム。０～２の数字を入力してください'));
 
 var message;
-if(answer=== number){
+if(answer === number){
     message='あたり！';
-}else if(answer<number){
+}else if(answer < number){
     message='残念！もっと大きいです';
-}else if(number>answer){
+}else if(number > answer){
 message='残念！もっと小さいです';
 }else{
-    message='０～３の数字を入力してください';
+    message = '０～３の数字を入力してください';
 }
-document.getElementById('choice').textContent =message;
+document.getElementById('choice').textContent = message;
